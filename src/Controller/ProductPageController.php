50b107233f7a4f4f4e0498be0fe167c62669a20d
CatCake <lucas.manot@gmail.com>
2023-02-15 15:21:13 +0100

    adding header , footer , carousel

--- src/Controller/ProductPageController.php
+++ src/Controller/ProductPageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Announce;
+use App\Form\AnnounceType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,11 @@ class ProductPageController extends AbstractController
     #[Route('/product/page', name: 'app_product_page')]
     public function index(): Response
     {
+        $announce = new Announce();
+        $form = $this->createForm(AnnounceType::class, $announce);
         return $this->render('product_page/index.html.twig', [
             'controller_name' => 'ProductPageController',
+            'form' => $form
         ]);
     }
 }
