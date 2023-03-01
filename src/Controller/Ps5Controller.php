956fa0f44d1fded14d3fdd464a05359872dfbe95
CatCake <lucas.manot@gmail.com>
2023-03-01 14:33:01 +0100

    modyfing link

--- src/Controller/Ps5Controller.php
+++ src/Controller/Ps5Controller.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class Ps5Controller extends AbstractController
 {
-    #[Route('/ps5', name: 'app_ps5')]
+    #[Route('/ps5', name: 'PS5')]
     public function index(): Response
     {
         return $this->render('ps5/index.html.twig', [
