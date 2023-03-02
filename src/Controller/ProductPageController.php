9905cbcafde7bf17833e7a283ef4459ebf300a2c
lucas <lucas@pop-os.localdomain>
2023-03-02 11:08:39 +0100

    adding details page for product

--- src/Controller/ProductPageController.php
+++ src/Controller/ProductPageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Announce;
+use App\Entity\Product;
 use App\Form\AnnounceType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductPageController extends AbstractController
 {
-    #[Route('/productpage', name: 'app_product_page')]
-    public function index(): Response
+    #[Route('/products/{id}', name: 'app_product_page.detail')]
+    public function index(Product $product): Response
     {
         return $this->render('product_page/index.html.twig', [
-            'controller_name' => 'ProductPageController',
+            'product' => $product,
         ]);
     }
 }
