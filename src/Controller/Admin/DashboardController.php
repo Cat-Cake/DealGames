0106887e212239d6ec0edbbfd24a5150e14feeb3
lucas <lucas@pop-os.localdomain>
2023-03-09 15:15:55 +0100

    Creating dashboard

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use App\Entity\Type;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+
+    public function __construct(private AdminUrlGenerator $adminUrlGenerator){
+
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+
+         $adminUrlGenerator = $this->adminUrlGenerator;
+         return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('DealGames Dashboard');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+         yield MenuItem::linkToCrud('Categories', 'fa-solid fa-cube', Product::class);
+         yield MenuItem::linkToCrud('Types', 'fa-solid fa-bars', Type::class);
+    }
+}
---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->onlyOnDetail(),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+            DateTimeField::new('createdAt')->onlyOnDetail(),
+        ];
+    }
+
+}
